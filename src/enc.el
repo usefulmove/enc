4103c2406b75213f33b419496f00440eda9099e7
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-23 21:32:35 -0700

    ...

---
+++ src/enc.el
@@ -0,0 +1,25 @@
+;;; enc.el --- emacs buffer encryption -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2023 Robert Duane Edmonds
+;;
+;; Author: Duane Edmonds <dedmonds@gmail.com>
+;; Maintainer: Duane Edmonds <dedmonds@gmail.com>
+;; Created: August 23, 2023
+;; Modified: August 23, 2023
+;; Version: 0.0.1
+;; Keywords: buffer encryption decryption
+;; Homepage: https://github.com/dedmonds/enc
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description: emacs buffer encryption
+;;
+;;; Code:
+
+
+
+(provide 'enc)
+;;; enc.el ends here
