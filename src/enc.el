cc933ac22deaa95f2fa69f35edc61d4a45dd0864
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-25 21:40:03 -0700

    ...

--- src/enc.el
+++ src/enc.el
@@ -1,11 +1,11 @@
 ;;; enc.el --- Buffer encryption -*- lexical-binding: t; -*-
 ;;
-;; Copyright (c) 2023 Robert Duane Edmonds
+;; Copyright (c) 2023 Duane Edmonds
 ;;
-;; Author: Duane Edmonds <dedmonds@gmail.com>
-;; Maintainer: Duane Edmonds <dedmonds@gmail.com>
+;; Author: Duane Edmonds <duane.edmonds@gmail.com>
+;; Maintainer: Duane Edmonds <duane.edmonds@gmail.com>
 ;; Created: August 23, 2023
-;; Modified: August 24, 2023
+;; Modified: August 25, 2023
 ;; Version: 0.0.1
 ;; Keywords: extensions files data processes tools
 ;; Homepage: https://github.com/dedmonds/enc
@@ -19,6 +19,8 @@
 ;;
 ;;; Code:
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; core functions
 
 ;; enc-read-buffer-contents :: nil -> string
 (defun enc-read-buffer-contents ()
@@ -36,10 +38,11 @@
             (t (+ base (mod (+ (- ord base) encryption-key) range)))))))
 
 ;; enc-encrypt-cstream :: encryption-key -> [char] -> [char]
-(defun enc-encrypt-cstream (encryption-key cstream)
+(defun enc-encrypt-cstream (encryption-key chars)
   "encrypt character stream"
   (let ((encrypt-char (enc-encrypt-char-with-key encryption-key)))
-    (reverse (mapcar encrypt-char cstream))))
+    (reverse (mapcar encrypt-char chars))))
+
 
 ;; enc-update-buffer :: string -> nil (impure)
 (defun enc-update-buffer (s)
@@ -47,24 +50,25 @@
   (delete-region (point-min) (point-max))
   (insert s))
 
+
 ;; enc-list-to-string :: [char] -> string
 (defun enc-list-to-string (lst)
   "join characters in list into a concatenated string"
   (apply 'concat (mapcar 'char-to-string lst)))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; interactive commands
 
-;; (interactive command)
 ;; enc-encrypt :: string -> nil (impure)
 (defun enc-encrypt (encryption-key)
   "Encrypt buffer contents."
   (interactive "sEnter encryption key: ")
-  (let* ((cstream (string-to-list (enc-read-buffer-contents)))
-         (encrypted (enc-encrypt-cstream (string-to-number encryption-key) cstream)))
+  (let* ((chars (string-to-list (enc-read-buffer-contents)))
+         (encrypted (enc-encrypt-cstream (string-to-number encryption-key) chars)))
     ; replace buffer contents with encrypted stream
     (enc-update-buffer (enc-list-to-string encrypted))))
 
-;; (interactive command)
 ;; enc-decrypt :: string -> nil (impure)
 (defun enc-decrypt (encryption-key)
   "Decrypt buffer contents."
