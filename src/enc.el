a059cda86aa904234fc95c27556e07a795b222f4
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-31 11:31:06 -0700

    minor refactor and updated docstrings

--- src/enc.el
+++ src/enc.el
@@ -5,8 +5,8 @@
 ;; Author: Duane Edmonds <duane.edmonds@gmail.com>
 ;; Maintainer: Duane Edmonds <duane.edmonds@gmail.com>
 ;; Created: August 23, 2023
-;; Modified: August 28, 2023
-;; Version: 0.0.5
+;; Modified: August 31, 2023
+;; Version: 0.0.6
 ;; Keywords: extensions files data processes tools
 ;; Homepage: https://github.com/usefulmove/enc
 ;; Package-Requires: ((emacs "24.3"))
@@ -34,7 +34,8 @@
 
 ;; enc-encrypt-char-with-key :: encryption-key -> (char -> char)
 (defun enc-encrypt-char-with-key (encryption-key)
-  "Key-specific single-character encryption - function decorator."
+  "Encrypt character using ENCRYPTION-KEY function decorator. Return key-specific
+  encryption function."
   (lambda (ord)
     (let ((base 32)
           (cap 127))
@@ -47,20 +48,20 @@
 
 ;; enc-encrypt-chars :: encryption-key -> [char] -> [char]
 (defun enc-encrypt-chars (encryption-key chars)
-  "Encrypt character stream."
+  "Encrypt character stream (CHARS) using ENCRYPTION-KEY."
   (let ((encrypt-char (enc-encrypt-char-with-key encryption-key)))
     (reverse (mapcar encrypt-char chars))))
 
 
 ;; enc-join-chars :: [char] -> string
 (defun enc-join-chars (chars)
-  "Join character list into a string."
+  "Join character list (CHARS) into a string."
   (apply 'string chars))
 
 
 ;; enc-string-negate :: string -> string
 (defun enc-string-negate (s)
-  "Negate value stored as string."
+  "Negate value stored as string (S)."
   (let* ((chars (string-to-list s)))
     (enc-join-chars (cond ((equal ?- (car chars)) (cdr chars))
                           (t (cons ?- chars))))))
@@ -68,7 +69,7 @@
 
 ;; enc-update-buffer :: string -> nil (impure)
 (defun enc-update-buffer (s)
-  "Replace the contents of the current buffer with character stream."
+  "Replace the contents of the current buffer with S."
   (delete-region (point-min) (point-max))
   (insert s))
 
@@ -79,10 +80,10 @@
 
 ;; enc-encrypt-buffer :: string -> nil (impure)
 (defun enc-encrypt-buffer (encryption-key-string)
-  "Encrypt contents of current buffer."
+  "Encrypt contents of current buffer using encryption key (ENCRYPTION-KEY-STRING)."
   (interactive "sEnter encryption key: ")
   (let ((encryption-key (string-to-number encryption-key-string)))
-    (cond ((_zero? encryption-key) (message "error: invalid key (enc)"))
+    (cond ((= 0 encryption-key) (message "error: invalid key (enc)"))
           (t (let ((encrypted-string (_thread (enc-read-buffer-contents)
                                        (lambda (chars)
                                           (enc-encrypt-chars encryption-key chars))
@@ -95,7 +96,7 @@
 
 ;; enc-decrypt-buffer :: string -> nil (impure)
 (defun enc-decrypt-buffer (encryption-key-string)
-  "Decrypt contents of current buffer."
+  "Decrypt contents of current buffer using encryption key (ENCRYPTION-KEY-STRING)."
   (interactive "sEnter decryption key: ")
   (enc-encrypt-buffer (enc-string-negate encryption-key-string)))
 
@@ -103,10 +104,10 @@
 
 ;; enc-encrypt-region :: string -> nil (impure)
 (defun enc-encrypt-region (encryption-key-string)
-  "Encrypt contents of selected region."
+  "Encrypt contents of selected region using encryption key (ENCRYPTION-KEY-STRING)."
   (interactive "sEnter encryption key: ")
   (let ((encryption-key (string-to-number encryption-key-string)))
-    (cond ((_zero? encryption-key) (message "error: invalid key (enc)"))
+    (cond ((= 0 encryption-key) (message "error: invalid key (enc)"))
           (t (let ((encrypted-string (_thread (buffer-substring
                                                 (region-beginning)
                                                 (region-end))
@@ -122,7 +123,7 @@
 
 ;; enc-decrypt-region :: string -> nil (impure)
 (defun enc-decrypt-region (encryption-key-string)
-  "Decrypt contents of selected region."
+  "Decrypt contents of selected region using encryption key (ENCRYPTION-KEY-STRING)."
   (interactive "sEnter decryption key: ")
   (enc-encrypt-region (enc-string-negate encryption-key-string)))
 
